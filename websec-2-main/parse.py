2b1b2035c5f5cb99c3764de5555dcb23a5a59337
Maria Telegina <t.marria@mail.ru>
2024-01-20 15:46:13 +0400

    links on university staff parsed

--- websec-2-main/parse.py
+++ websec-2-main/parse.py
@@ -2,14 +2,6 @@ import requests
 from bs4 import BeautifulSoup as BS
 import json
 
-def ParseFaculties():
-    page = requests.get("https://ssau.ru/rasp")
-    html = BS(page.content, 'html.parser')
-
-    for element in html.select(".faculties > .faculties__item"):
-        faculty = element.select('a')
-        print(faculty[0].text)
-
 def ParseGroups():
     groupsInfo = {}
     for course in range(1, 6):
@@ -28,20 +20,21 @@ def ParseTeachers():
     while True:
         webPage = requests.get(f"https://www.ssau.ru/staff?page={page}")
         html = BS(webPage.content, 'html.parser')
-        items = html.select(".list-group > li")
-        print(items)
-        # for item in items:
-            
+        items = html.select(".list-group > li > a")
+        for item in items:
+            # print(f"{item.text[:-1]}: {item['href']}")
+            teachersInfo[item.text[1:-1]] = item['href']
 
         if(len(items) == 0):
             break
-
-        print("____________END OF PAGE________________________________________________")
         page += 1
+        print(f"{page} страниц из 118 обработано", end="\r")
+
+    with open("TeachersInfo.json", "w") as file:
+        json.dump(teachersInfo, file, separators=(',\n', ': '), ensure_ascii=False)
 
 
 
 
-# ParseFaculties()
 ParseGroups()
-# ParseTeachers()
+ParseTeachers()
