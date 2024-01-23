20763630920dab59b8ca00940337c4a579769eea
Maria Telegina <t.marria@mail.ru>
2024-01-23 17:11:20 +0400

    server and script done

--- websec-2-main/index.js
+++ websec-2-main/index.js
@@ -1,22 +1,162 @@
-fetch("/groups")
-    .then(response => response.json())
-    .then(response => {
-        datalist = document.querySelector("#groups_teachers");
-        for (group in response) {
-            let optionGroup = document.createElement("option");
-            optionGroup.innerHTML = group;
-            datalist.appendChild(optionGroup)
+fetch('/rasp?groupId=531030143')
+    .then((data) => data.json())
+    .then((res) => {
+        console.log(res);
+        renderSchedule(res);
+        currentWeek = parseInt(res.currentWeek);
+        document.querySelector("#currentWeek").innerHTML = `${currentWeek} неделя`;
+        if (currentWeek == 1) {
+            document.querySelector("#previousButton").style.visibility = "hidden";
+        } else {
+            document.querySelector("#previousButton").style.visibility = "visible";
         }
     })
 
-// TODO: исправить кодировки
-fetch("/teachers")
-.then(response => response.json())
-.then(response => {
-    datalist = document.querySelector("#groups_teachers");
-    for (teacher in response) {
-        let optionTeacher = document.createElement("option");
-        optionTeacher.innerHTML = teacher;
-        datalist.appendChild(optionTeacher)
+fetch('/groups')
+    .then((data) => data.json())
+    .then((res) => {
+        console.log(res);
+        let selectElement = document.querySelector("#select");
+        for (group in res) {
+            let groupElement = document.createElement("option");
+            groupElement.innerHTML = group;
+            groupElement.setAttribute("value", res[group]);
+            selectElement.appendChild(groupElement);
+        }
+        selectElement.addEventListener("change", () => {
+            updateSchedule(selectElement.value);
+            console.log("LOOK:", selectElement.value)
+        })
+    })
+
+fetch('/teachers')
+    .then((data) => data.json())
+    .then((res) => {
+        console.log(res);
+        let selectElement = document.querySelector("#selectTeacher");
+        for (teacher in res) {
+            let teacherElement = document.createElement("option");
+            teacherElement.innerHTML = teacher;
+            teacherElement.setAttribute("value", res[teacher]);
+            selectElement.appendChild(teacherElement);
+        }
+        selectElement.addEventListener("change", () => {
+            updateSchedule(selectElement.value);
+        })
+    })
+
+let currentUrl = '/rasp?groupId=531030143';
+let currentWeek;
+let currentDay = new Date().getDay();
+let styles = "";
+let styleSheet = document.createElement("style");
+styleSheet.classList.add("schedule-style");
+
+
+function updateSchedule(url) {
+    currentUrl = url;
+    fetch(url)
+        .then((data) => data.json())
+        .then((res) => {
+            renderSchedule(res);
+            console.log(res);
+            currentWeek = parseInt(res.currentWeek);
+            document.querySelector("#currentWeek").innerHTML = `${currentWeek} неделя`;
+            if (currentWeek == 1) {
+                document.querySelector("#previousButton").style.visibility = "hidden";
+            } else {
+                document.querySelector("#previousButton").style.visibility = "visible";
+            }
+        })
+}
+
+function renderSchedule(data) {
+    let table = document.querySelector("#schedule");
+    table.innerHTML = "";
+    console.log(table);
+    let headers = table.insertRow();
+    headers.classList.add("first-row"); // класс верхней строчки
+    headers.insertCell().appendChild(document.createTextNode("Время"));
+
+    let ind = 0;
+    for (let date of data.dates) {
+        let cell = headers.insertCell();
+        cell.appendChild(document.createTextNode(date));
+        cell.classList.add(`column-${ind}`);
+        ind++;
     }
-})
+
+    ind = 0;
+    let days = data.daysOfSchedule;
+
+    for (let time of data.times) {
+        let row = table.insertRow();
+        row.classList.add("one-row"); // класс <tr>
+        row.insertCell().appendChild(document.createTextNode(time));
+
+        for (let day of days) {
+            if (ind > 5) {
+                break;
+            }
+            if (day.subject !== null) {
+                let infoToInsert = document.createElement("div");
+                let correctTeacher = JSON.parse(day.teacher);
+                infoToInsert.innerHTML = `${day.subject}<br>${day.place}<br>`;
+                let teacherElement;
+                if (correctTeacher.link !== null) {
+                    teacherElement = document.createElement("a");
+                    teacherElement.href = "#";
+                    teacherElement.innerHTML = correctTeacher.name;
+                    teacherElement.addEventListener('click', () => updateSchedule(correctTeacher.link));
+                } else {
+                    teacherElement = document.createElement("div");
+                    teacherElement.innerHTML = correctTeacher.name;
+                }
+                infoToInsert.classList.add("text-style1"); // класс <div>'а внутри ячейки <td>
+                infoToInsert.appendChild(teacherElement);
+                infoToInsert.appendChild(document.createElement("br"));
+                console.log(correctTeacher);
+                for (let group of day.groups) {
+                    let correctGroup = JSON.parse(group);
+                    let aNode;
+                    if (correctGroup.link !== null) {
+                        aNode = document.createElement("a");
+                        aNode.href = "#";
+                        aNode.innerHTML = correctGroup.name;
+                        aNode.addEventListener('click', () => updateSchedule(correctGroup.link));
+                    } else {
+                        aNode = document.createElement("div");
+                        aNode.innerHTML = correctGroup.name;
+                    }
+                    infoToInsert.appendChild(aNode);
+                    infoToInsert.appendChild(document.createElement("br"));
+                }
+                let cell = row.insertCell();
+                cell.classList.add(`column-${ind}`);
+                cell.appendChild(infoToInsert);
+                cell.classList.add("one-cell"); // класс <td> в таблице
+            } else {
+                let cell = row.insertCell();
+                cell.classList.add("one-cell"); // класс <td> в таблице
+                cell.classList.add(`column-${ind}`);
+            }
+            ind++;
+        }
+        days = days.slice(ind);
+        ind = 0;
+    }
+}
+
+function changePage(goNextPage) {
+    let ind = 0;
+    while (currentUrl[ind] !== "&" && ind <= 100) ind++;
+    if (currentUrl[ind] !== "&") {
+        currentUrl += `&selectedWeek=${goNextPage ? currentWeek + 1 + "" : currentWeek - 1 + ""}`;
+    } else {
+        currentUrl = currentUrl.slice(0, currentUrl.length - (currentWeek > 9 ? 2 : 1));
+        currentUrl += goNextPage ? currentWeek + 1 + "" : currentWeek - 1 + "";
+    }
+    console.log(currentWeek);
+    console.log(currentUrl);
+    updateSchedule(currentUrl);
+}
--- websec-2-main/server.js
+++ websec-2-main/server.js
@@ -1,13 +1,123 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const path = require('path');
+let HTMLParser = require('node-html-parser');
+let PORT = process.env.PORT || 3000;
+let XMLHttpRequest = require('xhr2');
+let express = require('express');
+let http = require('http'); // !
+let path = require('path'); // !
+let app = express();
+let server = http.Server(app); // !
+let bp = require('body-parser'); // !
+const fs = require("fs"); // !
 
 app.use(express.static(__dirname));
-
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 app.get('/', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(path.join(__dirname, 'index.html'));
 });
+server.listen(PORT, function() {
+    console.log('Server listening on 3000 . . .');
+});
+
+// https://ssau.ru/rasp?groupId=531030143&selectedWeek=22&selectedWeekday=1
+app.get('/rasp', (req, res) => {
+    console.log(req.url);
+    let request = new XMLHttpRequest();
+    let url = "https://ssau.ru" + req.url;
+    request.open("GET", url, true);
+    request.send(null);
+    request.onreadystatechange = () => {
+        if (request.readyState == 4) {
+            let schedule = {
+                dates: [],
+                daysOfSchedule: [],
+                times: []
+            };
+            let root = HTMLParser.parse(request.responseText);
+
+            // parse days with dates
+            for (let cell of root.querySelectorAll(".schedule__item + .schedule__head")) {
+                schedule.dates.push(cell.childNodes[0].innerText + cell.childNodes[1].innerText)
+            }
+
+            console.log(root.querySelector(".week-nav-current_week")?.innerText);
+
+            // parse all cells
+            for (let cell of root.querySelectorAll(".schedule__item")) {
+                if (cell.childNodes[0]?.childNodes.length > 3) {
+                    // console.log(cell.childNodes[0].childNodes[3].childNodes[1]);
+                    let groups = [];
+                    if (typeof req.query.staffId === "undefined") {
+                        cell.childNodes[0].childNodes[3].childNodes
+                            .filter((group) => group.innerText.trim() !== "")
+                            .map((group) => {
+                                let ind1 = 0;
+                                while (!isNumber(group.toString()[ind1]) && ind1 < 100) ind1++;
+                                let ind2 = ind1;
+                                while (isNumber(group.toString()[ind2]) && ind2 < 100) ind2++;
+                                while (group.toString()[ind1] !== "?" && ind1 > 0) ind1--;
+                                let id = group.toString().slice(ind1, ind2);
+                                groups.push(JSON.stringify({
+                                    name: group.innerText,
+                                    link: isNumber(id[id.length - 4]) ? `/rasp${id}` : null
+                                }))
+                            })
+                    } else {
+                        let groupsElements = cell.querySelectorAll("a")
+                            .filter((group) => group.innerText.trim() !== "")
+                            .map((group) => {
+                                let ind1 = 0;
+                                while (!isNumber(group.toString()[ind1]) && ind1 < 100) ind1++;
+                                let ind2 = ind1;
+                                while (isNumber(group.toString()[ind2]) && ind2 < 100) ind2++;
+                                while (group.toString()[ind1] !== "?" && ind1 > 0) ind1--;
+                                let id = group.toString().slice(ind1, ind2);
+                                groups.push(JSON.stringify({
+                                    name: group.innerText,
+                                    link: isNumber(id[id.length - 4]) ? `/rasp${id}` : null
+                                }))
+                            })
+                        console.log(groups);
+                    }
+
+                    let id = "";
+                    if (typeof req.query.staffId === "undefined") {
+                        let teacher = cell.childNodes[0].childNodes[2].childNodes;
+                        let ind1 = 5;
+                        while (!isNumber(teacher.toString()[ind1]) && ind1 < 100) ind1++;
+                        let ind2 = ind1;
+                        while (isNumber(teacher.toString()[ind2]) && ind2 < 100) ind2++;
+                        while (teacher.toString()[ind1] !== "?" && ind1 > 0) ind1--;
+                        id = teacher.toString().slice(ind1, ind2);
+                    }
+
+                    schedule.daysOfSchedule.push({
+                        subject: cell.childNodes[0].childNodes[0].innerText.slice(1),
+                        place: cell.childNodes[0].childNodes[1].innerText.slice(1),
+                        teacher: JSON.stringify(typeof req.query.staffId === "undefined" ? {
+                            name: cell.querySelector(".schedule__teacher")?.innerText ?? cell.childNodes[0].childNodes[2].childNodes[0].innerText,
+                            link: isNumber(id[id.length - 1]) ? `/rasp${id}` : null } : { name: "", link: "" }),
+                        groups: groups
+                    })
+                } else {
+                    schedule.daysOfSchedule.push({
+                        subject: null
+                    })
+                }
+            }
+
+            // parse times of first column
+            for (let cell of root.querySelectorAll(".schedule__time")) {
+                schedule.times.push(cell.childNodes[0].innerText + " - " + cell.childNodes[1].innerText);
+            }
+
+            // remove from field with subjects all headers with dates
+            schedule.daysOfSchedule = schedule.daysOfSchedule.slice(7);
+            schedule.currentWeek = root.querySelector(".week-nav-current_week")?.innerText.slice(1, 3).trim();
+            res.send(JSON.stringify(schedule));
+        }
+    };
+})
 
 app.get('/groups', function(request, response) {
 	response.sendFile(__dirname + '/GroupsInfo.json');
@@ -17,12 +127,14 @@ app.get('/teachers', function(request, response) {
 	response.sendFile(__dirname + '/TeachersInfo.json');
 });
 
-app.get('/schedule', function(request, response) {
-	response.sendFile(__dirname + '/Schedule.json');
-});
+function isNumber(char) {
+    if (typeof char !== 'string') {
+        return false;
+    }
 
-const server = app.listen(port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
-    console.log(`Server listening on port ${server.address().port}`);
-    console.log(`http://localhost:${port}`)
-}); 
+    if (char.trim() === '') {
+        return false;
+    }
+
+    return !isNaN(char);
+}
